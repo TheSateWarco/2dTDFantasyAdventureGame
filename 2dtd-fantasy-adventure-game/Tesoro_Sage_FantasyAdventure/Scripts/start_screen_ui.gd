5ceb9ad86540aa49a4935b4689244d8d5a8eb604
Sage Tesoro <satewarco@gmail.com>
2026-09-17 17:07:07 -0700

    audio ig

--- 2dtd-fantasy-adventure-game/Tesoro_Sage_FantasyAdventure/Scripts/start_screen_ui.gd
+++ 2dtd-fantasy-adventure-game/Tesoro_Sage_FantasyAdventure/Scripts/start_screen_ui.gd
@@ -1,11 +1,15 @@
 extends Control
 
+@onready var ClickSound: AudioStreamPlayer2D = $ClickSound
 
 func _on_start_button_pressed() -> void:
+	ClickSound.play()
+	await ClickSound.finished
 	get_tree().change_scene_to_file("res://Tesoro_Sage_FantasyAdventure/Scenes/Levels/MainGameLvl.tscn")
 
 
 func _on_quit_button_pressed() -> void:
+	ClickSound.play()
 	$QuitConfirmDialog.popup_centered()
 
 
