3347a7a824d2b132976a03e74c62df4dc8efa704
Sage Tesoro <satewarco@gmail.com>
2026-09-25 14:47:54 -0700

    more spirtes

--- 2dtd-fantasy-adventure-game/Tesoro_Sage_FantasyAdventure/Scenes/Characters/player.gd
+++ 2dtd-fantasy-adventure-game/Tesoro_Sage_FantasyAdventure/Scenes/Characters/player.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 @export var speed = 200
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var last_direction = "down"
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	
 	var input_direction = Input.get_vector("moveLeft", "moveRight", "moveUp",
 	"moveDown")
-
+	
 	velocity = input_direction * speed
-
+	update_direction(input_direction)
 	move_and_slide()
+	
+func update_movement_animation(input_direction):
+	if input_direction == Vector2.ZERO:
+		animated_sprite.play("idle_" + last_direction)
+	
+func update_direction(input_direction):
+	if input_direction.y < 0:
+		last_direction = "up"
+	elif input_direction.y > 0:
+		last_direction = "down"
+	elif input_direction.x < 0:
+		last_direction = "left"
+	elif input_direction.x > 0:
+		last_direction = "right"
